5db63e88ed9c824ebac271b97aca1bf42c5f7a4c
Pallavi <intern.pallavi@reapbenefit.org>
2020-02-25 20:50:43 +0530

    Taken absolute for arctan

--- ultrasonic_classification.py
+++ ultrasonic_classification.py
@@ -9,14 +9,14 @@ PIN_ECHO1 = 16
 PIN_TRIGGER2=18
 PIN_ECHO2=22
 
-x1o=22.55
-x2o=19.98
+x1o=6.72
+x2o=9.15
 threshold=1
 limit=300
 alpha=math.pi/3
-l=5.7
-theta_limit=10
-h_limit=3.5
+l=6
+theta_limit=30
+h_limit=5
 
 def distance(PIN_TRIGGER, PIN_ECHO):
       GPIO.setmode(GPIO.BOARD)
@@ -41,30 +41,26 @@ def distance(PIN_TRIGGER, PIN_ECHO):
 
 def calc(x1,x2):
       h=round((x2o-x2)*math.sin(alpha),2)
-      theta=(math.pi/2)-alpha-math.atan((x1-x2)/l)
+      theta=(math.pi/2)-alpha-math.atan((abs(x1-x2))/l)
       theta=round(math.degrees(theta),2)
       return h,theta
 
 def classify(x1,x2):
+    h,theta=calc(x1,x2)
+    print(h,theta)
     if((abs(x1-x1o)<=threshold) and (abs(x2-x2o)<=threshold)):
       print("Flat ground")
-    if((x1-x1o)<=(-threshold)and (x2-x2o)<=(-threshold) and abs(x1-x1o)-threshold<abs(x2-x2o)+threshold):
-      h,theta=calc(x1,x2)
-      print(h,theta)
-      if(h<h_limit):
-         print("Up stair")
-      elif(theta>0 and theta<theta_limit):
+    elif((x1-x1o)<=(-threshold)and (x2-x2o)<=(-threshold) and abs(x1-x1o)-threshold<abs(x2-x2o)+threshold):
+      if(theta>1 and theta<theta_limit):
          print("Up slope")
+      elif(h<h_limit):
+         print("Up stair")
       else:
          print("Obstacle")
-    if((x1-x1o)>threshold and (x2-x2o)>threshold and abs(x1-x1o)-threshold<abs(x2-x2o)+threshold):
+    elif((x1-x1o)>threshold and (x2-x2o)>threshold and abs(x1-x1o)-threshold<abs(x2-x2o)+threshold):
       print("Down stairs")
-      h,theta=calc(x1,x2)
-      print(h,theta)
-    if(abs(x1-x1o)>threshold and abs(x2-x2o)>threshold and x1-x1o-threshold>x2-x2o+threshold):
+    elif(abs(x1-x1o)>threshold and abs(x2-x2o)>threshold and x1-x1o-threshold>x2-x2o+threshold):
       print("Down slope")
-      h,theta=calc(x1,x2)
-      print(h,theta)
 
 try:
  while(1):
