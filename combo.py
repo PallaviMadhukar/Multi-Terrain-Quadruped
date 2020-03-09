eb33d04022075ca587f2e6e486063aebf20f31d9
PallaviMadhukar <35223728+PallaviMadhukar@users.noreply.github.com>
2020-03-09 19:31:45 +0530

    Import calibration, classification, creep, trot

---
+++ combo.py
@@ -0,0 +1,6 @@
+import calibration #main, distance, set_o_t
+import classification #main, distance, readings, classify, calc
+import creep #main, pinsetup, init, coxa, femur, tibia, begin, forward, backward, turn_left, turn_right, setServo, setServo_invert, leg1, leg2, leg3, leg4 
+import trot #main, pinsetup, begin, leg1_p2l, leg2_l2p, leg3_l2p, leg4_p2l, forward, setServo, setServo_invert, leg1, leg2, leg3, leg4
+
+
